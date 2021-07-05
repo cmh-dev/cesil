c4fc2c561e3cc6fb7e11af7175eb44844cd3efb2
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-07-05 10:33:57 +0100

    Removed error message formatting duplication

--- src/main/kotlin/uk/me/cmh/cesil/interpreter/Interpreter.kt
+++ src/main/kotlin/uk/me/cmh/cesil/interpreter/Interpreter.kt
@@ -3,13 +3,15 @@ package uk.me.cmh.cesil.interpreter
 class Interpreter(private val parser: Parser = Parser(), private val executor: Executor = Executor()) {
 
     fun executeProgram(program: String): ExecutionResult = when(val parserResult = parser.parse(program)) {
-        is ParserErrors -> ExecutionFailure(parserResult.errorMessages.map { "*** $it ***" })
+        is ParserErrors -> ExecutionFailure(parserResult.errorMessages.formatErrorMessages())
         is ParsedProgram ->
             when(val executionResult = executor.execute(parserResult.program)) {
-                is ExecutionFailure -> ExecutionFailure(executionResult.output.map { "*** $it ***" })
+                is ExecutionFailure -> ExecutionFailure(executionResult.output.formatErrorMessages())
                 is ExecutionSuccess -> executionResult
             }
     }
+
+    private fun List<String>.formatErrorMessages() = this.map { "*** $it ***" }
 
 }
 
