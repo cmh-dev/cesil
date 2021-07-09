cf65869e21cce6c3866cdb853318c591a333b01a
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-07-09 14:18:32 +0100

    Executor refactoring

--- src/main/kotlin/uk/me/cmh/cesil/interpreter/Executor.kt
+++ src/main/kotlin/uk/me/cmh/cesil/interpreter/Executor.kt
@@ -1,19 +1,24 @@
 package uk.me.cmh.cesil.interpreter
 
-class Executor {
+class Executor() {
+
+    private val outputBuffer = StringBuilder()
+    private var error = ""
+    private var accumulator = 0
+    private val variables = mutableMapOf<String, Int>()
+    private var currentInstructionIndex = 0
 
     fun execute(program: Program): ExecutionResult {
 
-        val outputBuffer = StringBuilder()
-        val errors = mutableListOf<String>()
-        var accumulator = 0
-        val variables = mutableMapOf<String, Int>()
+        outputBuffer.clear()
+        accumulator = 0
+        variables.clear()
+        currentInstructionIndex = 0
         val data = program.data.toMutableList()
 
-        var instructionIndex = 0
-        while (instructionIndex <= program.instructions.lastIndex) {
-            val instruction = program.instructions[instructionIndex]
-            instructionIndex++
+        while (currentInstructionIndex <= program.instructions.lastIndex && error.isBlank()) {
+            val instruction = program.instructions[currentInstructionIndex]
+            currentInstructionIndex++
             when(instruction.operator) {
                 Operator.PRINT -> outputBuffer.append(instruction.operand)
                 Operator.LINE -> outputBuffer.appendLine()
@@ -24,47 +29,34 @@ class Executor {
                 Operator.DIVIDE -> {
                     val divisor = getValue(instruction.operand, variables)
                     if (divisor == 0) {
-                        errors.add("DIVISION BY ZERO")
-                        break
+                        error = "DIVISION BY ZERO"
                     } else {
                         accumulator /= divisor
                     }
                 }
                 Operator.STORE -> variables[instruction.operand] = accumulator
                 Operator.LOAD -> accumulator = variables[instruction.operand] ?: 0
-                Operator.JUMP -> {
-                    instructionIndex = program.labeledInstructionIndexes[instruction.operand] ?: -1
-                    if (instructionIndex == -1) {
-                        errors.add("JUMP TO NON EXISTENT LABEL (${instruction.operand})")
-                        break
-                    }
-                }
-                Operator.JIZERO ->
-                    if (accumulator == 0) {
-                        instructionIndex = program.labeledInstructionIndexes[instruction.operand] ?: -1
-                        if (instructionIndex == -1) {
-                            errors.add("JUMP TO NON EXISTENT LABEL (${instruction.operand})")
-                            break
-                        }
-                    }
-                Operator.JINEG ->
-                    if (accumulator < 0) {
-                        instructionIndex = program.labeledInstructionIndexes[instruction.operand] ?: -1
-                        if (instructionIndex == -1) {
-                            errors.add("JUMP TO NON EXISTENT LABEL (${instruction.operand})")
-                            break
-                        }
-                    }
+                Operator.JUMP -> executeJumpInstruction(instruction.operand, program.labeledInstructionIndexes)
+                Operator.JIZERO ->  if (accumulator == 0) { executeJumpInstruction(instruction.operand, program.labeledInstructionIndexes) }
+                Operator.JINEG -> if (accumulator < 0) { executeJumpInstruction(instruction.operand, program.labeledInstructionIndexes) }
                 Operator.IN -> accumulator = data.removeFirst()
                 Operator.HALT -> break
             }
+            if (error.isNotBlank()) break
         }
 
         return when {
-            errors.isEmpty() -> ExecutionSuccess(outputBuffer.lines())
-            else -> ExecutionFailure(errors)
+            error.isEmpty() -> ExecutionSuccess(outputBuffer.lines())
+            else -> ExecutionFailure(listOf(error))
         }
 
+    }
+
+    private fun executeJumpInstruction(label: String, labeledInstructionIndexes: Map<String, Int>) {
+        currentInstructionIndex = labeledInstructionIndexes[label] ?: -1
+        if (currentInstructionIndex == -1) {
+            error = "JUMP TO NON EXISTENT LABEL ($label)"
+        }
     }
 
     private fun getValue(operand: String, variables: Map<String, Int>) : Int {
