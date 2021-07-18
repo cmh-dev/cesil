92c1821faee9fd43cc395ceff211371df69b8319
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-07-18 21:58:27 +0100

    Running of a program from the web application

--- src/main/kotlin/uk/me/cmh/cesil/web/CesilServer.kt
+++ src/main/kotlin/uk/me/cmh/cesil/web/CesilServer.kt
@@ -2,6 +2,8 @@ package uk.me.cmh.cesil.web
 
 import com.natpryce.konfig.*
 import org.http4k.core.*
+import org.http4k.core.body.form
+import org.http4k.core.body.formAsMap
 import org.http4k.routing.ResourceLoader.Companion.Classpath
 
 import org.http4k.routing.bind
@@ -12,14 +14,20 @@ import org.http4k.server.Jetty
 import org.http4k.server.asServer
 import org.http4k.template.ThymeleafTemplates
 import org.http4k.template.ViewModel
+import uk.me.cmh.cesil.interpreter.Interpreter
 
 val portKey = Key("port", intType)
 val config = EnvironmentVariables() overriding ConfigurationProperties.fromResource("default.properties")
 
-
 data class MainViewModel(val sourceCode: String = "") : ViewModel {
     override fun template(): String {
         return "templates/main.html"
+    }
+}
+
+data class RunResultsViewModel(val results: List<String>) : ViewModel {
+    override fun template(): String {
+        return "templates/run-results.html"
     }
 }
 
@@ -42,7 +50,25 @@ fun cesilServerHander(): HttpHandler {
                     )
                 )
                 .header("Content-Type", ContentType.TEXT_HTML.toHeaderValue())
+        },
+        "/run-program" bind Method.POST to { request ->
+            val interpreter = Interpreter()
+            val parameters: Map<String, List<String?>> = request.formAsMap()
+            val sourceCode = parameters.get("sourcecode")?.get(0) ?: ""
+            val executionResult = interpreter.executeProgram(sourceCode)
+            val results = sourceCode.lines() + listOf("", "RESULTS:") + executionResult.output
+            Response(Status.OK)
+                .body(
+                    renderer.invoke(
+                        RunResultsViewModel(results)
+                    )
+                )
+                .header("Content-Type", ContentType.TEXT_HTML.toHeaderValue())
         }
     )
 
+}
+
+fun main() {
+    cesilServer().start()
 }
--- src/test/kotlin/uk/me/cmh/cesil/web/CesilServerTest.kt
+++ src/test/kotlin/uk/me/cmh/cesil/web/CesilServerTest.kt
@@ -1,14 +1,14 @@
 package uk.me.cmh.cesil.web
 
 import com.gargoylesoftware.htmlunit.WebClient
-import com.gargoylesoftware.htmlunit.html.HtmlPage
+import com.gargoylesoftware.htmlunit.html.*
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLTextAreaElement
 import org.http4k.server.Http4kServer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.io.Closeable
-import java.io.FileWriter
+
 
 class CesilServerTest {
 
@@ -37,9 +37,28 @@ class CesilServerTest {
     @Test
     fun `when root page is requested and html page is returned with an empty editor`() {
         val page = webClient.getPage<HtmlPage>("http://localhost:8080/")
-        assertEquals("", page.getElementById("editor").textContent)
+        val editor = page.getElementById("editor") as HtmlTextArea
+        assertEquals("", editor.text)
     }
 
-
+    @Test
+    fun `when code is run and html page is returned with the results`() {
+        val editorPage = webClient.getPage<HtmlPage>("http://localhost:8080/")
+        val editor = editorPage.getElementById("editor") as HtmlTextArea
+        editor.text = """PRINT "HELLO WORLD"
+                      HALT
+                      %
+                      *"""
+        val resultsPage = (editorPage.getElementById("button-run") as HtmlButton).click<HtmlPage>()
+        val resultsTable = resultsPage.getElementById("table-results") as HtmlTable
+        val results = resultsTable.rows.map { row -> row.getCell(0).textContent }
+        assertEquals("PRINT \"HELLO WORLD\"", results[0].trim())
+        assertEquals("HALT", results[1].trim())
+        assertEquals("%", results[2].trim())
+        assertEquals("*", results[3].trim())
+        assertEquals("", results[4].trim())
+        assertEquals("RESULTS:", results[5].trim())
+        assertEquals("HELLO WORLD", results[6].trim())
+    }
 
 }
