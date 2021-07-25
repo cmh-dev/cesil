6108340e4898c6ab7b8a97d8364fbe9bb9dd1138
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-07-25 22:11:00 +0100

    Executor made thread safe

--- src/main/kotlin/uk/me/cmh/cesil/interpreter/Executor.kt
+++ src/main/kotlin/uk/me/cmh/cesil/interpreter/Executor.kt
@@ -2,81 +2,138 @@ package uk.me.cmh.cesil.interpreter
 
 class Executor {
 
-    private val outputBuffer = StringBuilder()
-    private var error = ""
-    private var accumulator = 0
-    private val variables = mutableMapOf<String, Int>()
-    private var currentInstructionIndex = 0
+    private data class ProgramState(
+        val output: String = "",
+        val error: String = "",
+        val accumulator: Int = 0,
+        val variables: Map<String, Int> = mapOf(),
+        val data: List<Int> = listOf(),
+        val currentInstructionIndex: Int = 0
+    )
 
     fun execute(program: Program): ExecutionResult {
 
-        outputBuffer.clear()
-        accumulator = 0
-        variables.clear()
-        currentInstructionIndex = 0
-        val data = program.data.toMutableList()
+        var programState = ProgramState(data = program.data)
 
-        while (currentInstructionIndex <= program.instructions.lastIndex && error.isBlank()) {
-            val instruction = program.instructions[currentInstructionIndex]
-            currentInstructionIndex++
-            when(instruction.operator) {
-                Operator.PRINT -> outputBuffer.append(instruction.operand)
-                Operator.LINE -> outputBuffer.appendLine()
-                Operator.OUT -> outputBuffer.append(accumulator)
-                Operator.ADD -> executeInstructionUsingValue(instruction)
-                Operator.SUBTRACT -> executeInstructionUsingValue(instruction)
-                Operator.MULTIPLY -> executeInstructionUsingValue(instruction)
-                Operator.DIVIDE -> executeInstructionUsingValue(instruction)
-                Operator.STORE -> variables[instruction.operand] = accumulator
-                Operator.LOAD -> executeInstructionUsingValue(instruction)
-                Operator.JUMP -> executeJumpInstruction(instruction.operand, program.labeledInstructionIndexes)
-                Operator.JIZERO ->  if (accumulator == 0) { executeJumpInstruction(instruction.operand, program.labeledInstructionIndexes) }
-                Operator.JINEG -> if (accumulator < 0) { executeJumpInstruction(instruction.operand, program.labeledInstructionIndexes) }
-                Operator.IN -> if (data.isNotEmpty()) { accumulator = data.removeFirst() } else error = "PROGRAM REQUIRES MORE DATA"
+        while (programState.currentInstructionIndex <= program.instructions.lastIndex && programState.error.isBlank()) {
+            val instruction = program.instructions[programState.currentInstructionIndex]
+            programState = when (instruction.operator) {
+                Operator.PRINT -> getNextProgramStateWithOutputAddition(instruction.operand, programState)
+                Operator.LINE -> getNextProgramStateWithOutputAddition(System.lineSeparator(), programState)
+                Operator.OUT -> getNextProgramStateWithOutputAddition(programState.accumulator.toString(), programState)
+                Operator.ADD -> executeInstructionUsingValue(instruction, programState)
+                Operator.SUBTRACT -> executeInstructionUsingValue(instruction, programState)
+                Operator.MULTIPLY -> executeInstructionUsingValue(instruction, programState)
+                Operator.DIVIDE -> executeInstructionUsingValue(instruction, programState)
+                Operator.STORE -> programState.copy(
+                    variables = mapOf(
+                        *programState.variables.entries.map { it.key to it.value }.toTypedArray(),
+                        instruction.operand to programState.accumulator
+                    ), currentInstructionIndex = programState.currentInstructionIndex + 1
+                )
+                Operator.LOAD -> executeInstructionUsingValue(instruction, programState)
+                Operator.JUMP -> executeJumpInstruction(
+                    instruction.operand,
+                    program.labeledInstructionIndexes,
+                    programState
+                )
+                Operator.JIZERO -> if (programState.accumulator == 0) {
+                    executeJumpInstruction(instruction.operand, program.labeledInstructionIndexes, programState)
+                } else {
+                    programState.copy(currentInstructionIndex = programState.currentInstructionIndex + 1)
+                }
+                Operator.JINEG -> if (programState.accumulator < 0) {
+                    executeJumpInstruction(instruction.operand, program.labeledInstructionIndexes, programState)
+                } else {
+                    programState.copy(currentInstructionIndex = programState.currentInstructionIndex + 1)
+                }
+                Operator.IN -> if (programState.data.isNotEmpty()) {
+                    programState.copy(accumulator = programState.data.first(), data = programState.data.drop(1), currentInstructionIndex = programState.currentInstructionIndex + 1)
+                } else {
+                    getNextProgramStateWithError("PROGRAM REQUIRES MORE DATA", programState)
+                }
                 Operator.HALT -> break
+                Operator.INVALID_OPERATOR -> programState
             }
-            if (error.isNotBlank()) break
+            if (programState.error.isNotBlank()) break
         }
 
         return when {
-            error.isEmpty() -> ExecutionSuccess(outputBuffer.lines())
-            else -> ExecutionFailure(listOf(error))
+            programState.error.isEmpty() -> ExecutionSuccess(programState.output.lines())
+            else -> ExecutionFailure(listOf(programState.error))
         }
 
     }
 
-    private fun executeInstructionUsingValue(instruction: Instruction) {
+    private fun executeInstructionUsingValue(instruction: Instruction, currentProgramState: ProgramState): ProgramState {
         val value = when {
             instruction.operand.matches(Regex("^(\\+|-)*\\d*\$")) -> instruction.operand.toInt()
-            else -> variables[instruction.operand]
+            else -> currentProgramState.variables[instruction.operand]
         }
-        if (value != null) {
-            when (instruction.operator) {
-                Operator.ADD -> accumulator += value
-                Operator.SUBTRACT -> accumulator -= value
-                Operator.MULTIPLY -> accumulator *= value
-                Operator.DIVIDE -> {
+        return if (value != null) {
+            return when {
+                instruction.operator == Operator.ADD -> getNextProgramStateWithNewAccumulatorValue(
+                    currentProgramState.accumulator + value,
+                    currentProgramState
+                )
+                instruction.operator == Operator.SUBTRACT -> getNextProgramStateWithNewAccumulatorValue(
+                    currentProgramState.accumulator - value,
+                    currentProgramState
+                )
+                instruction.operator == Operator.MULTIPLY -> getNextProgramStateWithNewAccumulatorValue(
+                    currentProgramState.accumulator * value,
+                    currentProgramState
+                )
+                instruction.operator == Operator.DIVIDE -> {
                     if (value == 0) {
-                        error = "DIVISION BY ZERO"
+                        getNextProgramStateWithError("DIVISION BY ZERO", currentProgramState)
                     } else {
-                        accumulator /= value
+                        getNextProgramStateWithNewAccumulatorValue(currentProgramState.accumulator / value, currentProgramState)
                     }
                 }
-                Operator.LOAD -> accumulator = value
+                instruction.operator == Operator.LOAD -> getNextProgramStateWithNewAccumulatorValue(value, currentProgramState)
+                else -> currentProgramState
             }
         } else {
-            error = buildNonExistentVariableMessage(instruction.operand)
+            getNextProgramStateWithError(buildNonExistentVariableMessage(instruction.operand), currentProgramState)
         }
     }
+
+    private fun executeJumpInstruction(
+        label: String,
+        labeledInstructionIndexes: Map<String, Int>,
+        currentProgramState: ProgramState
+    ): ProgramState {
+        val instructionIndex = labeledInstructionIndexes[label] ?: -1
+        return when (instructionIndex) {
+            -1 -> getNextProgramStateWithError("JUMP TO NON EXISTENT LABEL ($label)", currentProgramState)
+            else -> currentProgramState.copy(currentInstructionIndex = instructionIndex)
+        }
+    }
+
+    private fun getNextProgramStateWithOutputAddition(
+        additionalOutput: String,
+        currentProgramState: ProgramState
+    ): ProgramState =
+        currentProgramState.copy(
+            output = "${currentProgramState.output}${additionalOutput}",
+            currentInstructionIndex = currentProgramState.currentInstructionIndex + 1
+        )
+
+    private fun getNextProgramStateWithNewAccumulatorValue(
+        accumulatorValue: Int,
+        currentProgramState: ProgramState
+    ): ProgramState =
+        currentProgramState.copy(
+            accumulator = accumulatorValue,
+            currentInstructionIndex = currentProgramState.currentInstructionIndex + 1
+        )
+
+    private fun getNextProgramStateWithError(errorValue: String, currentProgramState: ProgramState): ProgramState =
+        currentProgramState.copy(error = errorValue, currentInstructionIndex = currentProgramState.currentInstructionIndex + 1)
+
 
     private fun buildNonExistentVariableMessage(variableName: String) = "NON EXISTENT VARIABLE ($variableName)"
-
-    private fun executeJumpInstruction(label: String, labeledInstructionIndexes: Map<String, Int>) {
-        currentInstructionIndex = labeledInstructionIndexes[label] ?: -1
-        if (currentInstructionIndex == -1) {
-            error = "JUMP TO NON EXISTENT LABEL ($label)"
-        }
-    }
 
 }
 
