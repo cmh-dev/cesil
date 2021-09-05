be1989ee32ca8022ae39338939b26d0509d43957
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-09-05 17:51:08 +0100

    Fixing code analysis warnings

--- cesil-interpreter/src/main/kotlin/uk/me/cmh/cesil/interpreter/Program.kt
+++ cesil-interpreter/src/main/kotlin/uk/me/cmh/cesil/interpreter/Program.kt
@@ -1,13 +1,11 @@
 package uk.me.cmh.cesil.interpreter
 
-class Program(instructions: List<Instruction>, data: List<Int>) {
+class Program(val instructions: List<Instruction>, val data: List<Int>) {
 
-    val instructions: List<Instruction> = instructions
     val labeledInstructionIndexes: Map<String, Int> = instructions
         .mapIndexed { index, instruction ->  instruction.label to index }
         .filterNot { it.first.isBlank() }
         .toMap()
-    val data: List<Int> = data
 
 }
 
