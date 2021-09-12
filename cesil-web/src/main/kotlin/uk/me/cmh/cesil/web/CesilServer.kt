8f209b6497a0145151284d13fb087c89f56bdd4a
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-09-12 11:49:15 +0100

    Emulator changed to Interpreter in web app

--- cesil-web/src/main/kotlin/uk/me/cmh/cesil/web/CesilServer.kt
+++ cesil-web/src/main/kotlin/uk/me/cmh/cesil/web/CesilServer.kt
@@ -15,11 +15,11 @@ import uk.me.cmh.cesil.interpreter.Interpreter
 val portKey = Key("port", intType)
 val config = EnvironmentVariables() overriding ConfigurationProperties.fromResource("default.properties")
 
-data class EmulatorModel(val sourceCode: String = "", val output: String = "")
-
 fun main() {
     cesilServer().start()
 }
+
+private const val INTERPRETER_MODEL_AND_TEMPLATE = "interpreter"
 
 fun cesilServer() = embeddedServer(Netty, port = config[portKey]) {
 
@@ -36,12 +36,12 @@ fun cesilServer() = embeddedServer(Netty, port = config[portKey]) {
             resources("static")
         }
         get("/") {
-            call.respond(ThymeleafContent("emulator", mapOf("emulator" to EmulatorModel())))
+            call.respond(ThymeleafContent(INTERPRETER_MODEL_AND_TEMPLATE, mapOf(INTERPRETER_MODEL_AND_TEMPLATE to InterpreterModel())))
         }
         post("/") {
             val sourceCode = call.receiveParameters()["sourceCode"] ?: ""
             val output = Interpreter().executeProgram(sourceCode).output.joinToString(separator = "\n")
-            call.respond(ThymeleafContent("emulator", mapOf("emulator" to EmulatorModel(sourceCode, output))))
+            call.respond(ThymeleafContent(INTERPRETER_MODEL_AND_TEMPLATE, mapOf(INTERPRETER_MODEL_AND_TEMPLATE to InterpreterModel(sourceCode, output))))
 
         }
         get("/docs") {
