62ec34e636440b51e3c1c3d245dd22c4c2a016eb
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-10-30 11:12:35 +0100

    Set-up of CLI module

---
+++ cesil-cli/src/main/kotlin/uk/me/cmh/cesil/cli/CliApp.kt
@@ -0,0 +1,5 @@
+package uk.me.cmh.cesil.cli
+
+fun main() {
+    println("Hello World!")
+}
---
+++ cesil-cli/src/test/kotlin/uk/me/cmh/cesil/cli/CliAppTest.kt
@@ -0,0 +1,32 @@
+package uk.me.cmh.cesil.cli
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+
+class CliAppTest {
+
+    private val standardOut = System.out
+    private val outputStreamCaptor = ByteArrayOutputStream()
+
+    @BeforeEach
+    fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        System.setOut(standardOut)
+    }
+
+    @Test
+    fun `when the app is run output is produced`() {
+        main()
+        assertEquals("Hello World!", outputStreamCaptor.toString().trim())
+    }
+
+}
