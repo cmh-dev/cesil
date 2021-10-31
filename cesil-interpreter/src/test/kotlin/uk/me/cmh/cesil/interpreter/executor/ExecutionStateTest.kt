47a44253a3f7c555ca465d3c2ed36cfb4978c0ab
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-10-31 22:12:50 +0000

    Removing missing import

--- cesil-interpreter/src/test/kotlin/uk/me/cmh/cesil/interpreter/executor/ExecutionStateTest.kt
+++ cesil-interpreter/src/test/kotlin/uk/me/cmh/cesil/interpreter/executor/ExecutionStateTest.kt
@@ -1,7 +1,6 @@
 package uk.me.cmh.cesil.interpreter.executor
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class ExecutionStateTest {
