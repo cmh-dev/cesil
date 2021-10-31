b5048bd639d1041cfa78b49f3f57e704f7778fe9
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-10-31 22:12:01 +0000

    Refactoring CLI app

--- cesil-cli/src/main/kotlin/uk/me/cmh/cesil/cli/CliApp.kt
+++ cesil-cli/src/main/kotlin/uk/me/cmh/cesil/cli/CliApp.kt
@@ -3,17 +3,19 @@ package uk.me.cmh.cesil.cli
 import uk.me.cmh.cesil.interpreter.Interpreter
 import java.io.File
 
-fun main(args: Array<String>) {
-    if (args.size == 0) {
-        println("*** MISSING SOURCE CODE FILE ARGUMENT ***")
-    } else {
-        val sourceCodeFile = File(args[0])
-        if (sourceCodeFile.exists()) {
-            val interpreter = Interpreter()
-            val executionResult = interpreter.executeProgram(sourceCodeFile.readText())
-            executionResult.output.forEach { println(it) }
-        } else {
-            println("*** SOURCE CODE FILE NOT FOUND ***")
-        }
+fun main(args: Array<String>) =
+    if (args.isEmpty()) println("*** MISSING SOURCE CODE FILE ARGUMENT ***") else processSourceCodeFile(args[0])
+
+private fun processSourceCodeFile(sourceCodeFilePath: String) {
+    val sourceCodeFile = File(sourceCodeFilePath)
+    when {
+        sourceCodeFile.exists() -> executeProgram(sourceCodeFile)
+        else -> println("*** SOURCE CODE FILE NOT FOUND ***")
     }
+}
+
+private fun executeProgram(sourceCodeFile: File) {
+    val interpreter = Interpreter()
+    val executionResult = interpreter.executeProgram(sourceCodeFile.readText())
+    executionResult.output.forEach { println(it) }
 }
