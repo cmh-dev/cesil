c71a142f8ade78179de17f756322020dac25c7cc
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2021-10-31 10:48:42 +0000

    CLI app excutes a program

--- cesil-cli/src/main/kotlin/uk/me/cmh/cesil/cli/CliApp.kt
+++ cesil-cli/src/main/kotlin/uk/me/cmh/cesil/cli/CliApp.kt
@@ -1,5 +1,11 @@
 package uk.me.cmh.cesil.cli
 
+import uk.me.cmh.cesil.interpreter.Interpreter
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("THE ANSWER IS 42")
+    val sourceCodeFile = File(args[0])
+    val interpreter = Interpreter()
+    val executionResult = interpreter.executeProgram(sourceCodeFile.readText())
+    executionResult.output.forEach { println(it) }
 }
--- cesil-cli/src/test/kotlin/uk/me/cmh/cesil/cli/CliAppTest.kt
+++ cesil-cli/src/test/kotlin/uk/me/cmh/cesil/cli/CliAppTest.kt
@@ -3,6 +3,7 @@ package uk.me.cmh.cesil.cli
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
@@ -28,6 +29,14 @@ class CliAppTest {
         val testSourceCodeFilePath = CliAppTest::class.java.getResource("/test-source-code.txt").path
         main(arrayOf(testSourceCodeFilePath))
         assertEquals("THE ANSWER IS 42", outputStreamCaptor.toString().trim())
+    }
+
+    @Disabled
+    @Test
+    fun `when an attempt to execute a missing source code file is undertaken an error is returned`() {
+        val testSourceCodeFilePath = CliAppTest::class.java.getResource("/missing-source-code.txt").path
+        main(arrayOf(testSourceCodeFilePath))
+        assertEquals("*** SOURCE CODE FILE NOT FOUND ***", outputStreamCaptor.toString().trim())
     }
 
 }
